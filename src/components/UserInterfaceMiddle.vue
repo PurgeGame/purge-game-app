e72f6481d27fb28af971ee832d88d586ee70ff88
Juan Dough <arctos@arctos.digital>
2022-04-03 16:16:56 -0400

    Ethers stuff

--- src/components/UserInterfaceMiddle.vue
+++ src/components/UserInterfaceMiddle.vue
@@ -1,38 +1,43 @@
-<!-- 
-  TO DO
-  --------------------
-  Make 'Mint' button display conditional, based on... ?
-  Table with owned traits, # purged, $ value
-  Connect Discord button & Referral Code field in this column?
--->
-
 <script setup>
+import { ref, onMounted } from "vue";
+import { ethers } from "ethers";
+import { wallet } from "../store.js";
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+const walletBalance = ref(null);
+const currentBlock = ref(null);
+
+async function doEthersStuff() {
+  walletBalance.value = ethers.utils.formatEther(
+    await provider.getBalance(wallet.address)
+  );
+  currentBlock.value = await provider.getBlockNumber();
+}
+
+async function signSomething() {
+  await signer.signMessage("John Hancock");
+}
+
+onMounted(() => {
+  doEthersStuff();
+});
 </script>
 
 <template>
   <div class="relative h-full mx-2">
-    <img src="/newbanner.png" class="w-full" />
-    <button
-      class="
-        block
-        bg-black
-        mx-auto
-        my-12
-        px-4
-        py-2
-        rounded-lg
-        text-xl text-amber-500
-        border border-amber-600
-        hover:text-amber-400 hover:ring
-        ring-amber-500
-        font-bold
-      "
-    >
-      Mint Game Piece
-    </button>
-    <div
-      class="block bg-black w-auto p-4 border-2 border-red-900 rounded-lg"
-    ></div>
+    <img src="/newbanner.png" class="w-full mb-10" />
+
+    Fake ethrums balance: {{ walletBalance }}
+    <br />
+    Current block: {{ currentBlock }}
+
+    <p class="mt-10">
+      <button @click="signSomething()" class="p-2 bg-black border rounded">
+        Click to Sign
+      </button>
+    </p>
 
     <div class="absolute bottom-0 w-full my-2 text-center lg:hidden">
       Traits <img src="/swipe.png" class="inline px-1" /> NFT's
