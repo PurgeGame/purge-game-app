5eedd1538aa1f287bf0d123b03c77022b5600385
PurgeGame <99993771+PurgeGame@users.noreply.github.com>
2022-04-23 19:01:26 -0500

    prize pool data

---
+++ src/components/LeftMint.vue
@@ -0,0 +1,71 @@
+<!-- This whole component is hideous and hard to understand and needs help -->
+
+<script setup>
+import { ref, onMounted, computed } from "vue";
+import { ethers } from "ethers";
+import {
+  prizepool,
+  contract,
+  cost,
+} from "../store.js";
+
+const totalMinted = ref(null)
+const totalMAP = ref(null)
+const ethPrice = ref(null)
+
+async function doEthersStuff() {
+    prizepool.total = await contract.PrizePool() / 1e18
+    totalMinted.value = await contract.totalMinted()
+    totalMAP.value = await contract.totalSupply() - totalMinted.value
+    let response = await fetch('https://api.coinbase.com/v2/prices/ETH-USD/buy');
+    let data = await response.json();
+    ethPrice.value = data.data.amount
+}
+
+function whitepaper(){
+    window.open('https://purge.game/concept.html');
+}
+
+// function signSomething() {
+//   let number = document.getElementById("number").value;
+//   let referrer = document.getElementById("string").value;
+//   mintAndPurge(number, referrer);
+// }
+
+onMounted(() => {
+  doEthersStuff();
+});
+
+</script>
+
+<template>
+    <div
+      class="
+        mt-4
+        p-1
+        w-[75%]
+        h-[150px]
+        overflow-hidden
+        bg-black
+        border-2 border-red-700
+        rounded-lg
+      "
+      style = "
+  position: relative;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);"
+    >
+    <p>
+    Total Prizepool : {{prizepool.total}} eth (${{parseInt(prizepool.total * ethPrice)}})
+    </p>
+    <p>
+    Grand Prize : {{totalMinted * cost * .05 /1e18}} eth (${{parseInt(totalMinted * cost * .05 * ethPrice /1e18)}})
+    </p>
+    <p>
+    MAP Jackpot : {{totalMAP * cost * .05 /1e18}} eth (${{parseInt(totalMAP * cost * .05 * ethPrice /1e18)}})
+    </p>
+    </div>
+
+</template>
